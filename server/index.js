d3373f88daa0992255131b497d073d1653f419f9
Minh Vy ha <minhvy.ha@outlook.com>
2022-08-30 19:48:13 +1000

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require('mongoose')
+
 const port = 5001;
 const app = express();
+mongoose.connect("mongodb+srv://minhvy:<password>@cluster0.lo27vwn.mongodb.net/?retryWrites=true&w=majority")
 
 app.use(express.json());
 app.use(cors());
