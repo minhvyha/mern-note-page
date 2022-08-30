f2328c5af5244f5d2538064ec07d2e51f3887cca
Minh Vy ha <minhvy.ha@outlook.com>
2022-08-30 19:38:02 +1000

    update

--- server/index.js
+++ server/index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 5001;
-const { router } = require("./api/v1/index");
 
 app.use(express.json());
 app.use(cors());
-app.use("/notes", router);
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+
+
 
 app.listen(port, () => {
   console.log("hello, world");
---
+++ server/models/Node.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const NoteSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        require: true
+    },
+    text: {
+        type: String,
+        require: true
+    },
+    link: {
+        type: String,
+        require: true
+    }
+})
