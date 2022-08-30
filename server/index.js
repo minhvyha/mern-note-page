b5a184013a457975ec188c54422c6a6922a135cf
Minh Vy ha <minhvy.ha@outlook.com>
2022-08-30 20:10:41 +1000

    update

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 const mongoose = require('mongoose')
 const NoteModel = require('./models/Note')
 
+
+
 const port = 5001;
 const app = express();
-mongoose.connect("mongodb+srv://minhvy:<password>@cluster0.lo27vwn.mongodb.net/notedatabse?retryWrites=true&w=majority")
+mongoose.connect(`mongodb+srv://minhvy:SH8Z5aEju51aQUPT@cluster0.lo27vwn.mongodb.net/notedatabse?retryWrites=true&w=majority`)
 
 app.use(express.json());
 app.use(cors());
@@ -22,6 +24,9 @@ app.use('/getNote', (req, res) =>{
   })
 })
 
+app.get('/', (req, res) =>{
+  res.send('hello world')
+})
 
 app.listen(port, () => {
   console.log("hello, world");
