709fd54e1041bd928ade417d4d88814062fe63dc
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-03 19:19:40 +1000

    update

---
+++ client/src/components/Form.tsx
@@ -0,0 +1,14 @@
+import React, { FC, FocusEvent } from "react";
+
+type Props = {
+  onCreate: { text: string; link: string };
+};
+
+const Form: FC<Props> = ({ onCreate }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+export default Form;
