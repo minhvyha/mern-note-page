da2e740f6129c7d472744c0f71b5786b9ba8079c
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-03 22:01:47 +1000

    update

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -6,17 +6,22 @@ type Props = {
 
 const Form: FC<Props> = ({ onCreate }) => {
   return (
-    <>
-      <div>
-        <span>Text: </span>
-        <input type="text" />
+    <div className="create-container">
+      <div className="create-form">
+        <div>
+          <span>Text: </span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>Link: </span>
+          <input id="link-input" type="text" />
+        </div>
+        <div>
+          <button>Close</button>
+          <button>Create</button>
+        </div>
       </div>
-      <div>
-        <span>Link: </span>
-        <input id="link-input" type="text" />
-      </div>
-      <button>Create</button>
-    </>
+    </div>
   );
 };
 
