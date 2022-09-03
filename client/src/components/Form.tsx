df848a1152570b40c98663162246428d20199c91
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-03 21:51:54 +1000

    update

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,15 +1,22 @@
 import React, { FC, FocusEvent } from "react";
 
 type Props = {
-  onCreate: (text: string, link: string) => void
+  onCreate: (text: string, link: string) => void;
 };
 
 const Form: FC<Props> = ({ onCreate }) => {
-    return (
-        <>
-          <input type="text" />
-        </>
-    )
+  return (
+    <>
+      <div>
+        <span>Text</span>
+        <input type="text" />
+      </div>
+      <div>
+        <span>Link</span>
+        <input id="link-input" type="text" />
+      </div>
+    </>
+  );
 };
 
 export default Form;
